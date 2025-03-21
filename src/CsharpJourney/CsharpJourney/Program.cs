d8095209790c9bb820b7bb86c5a1b7aa7c25255e
Park YongJune <redplus@redplus.net>
2025-03-21 15:50:29 +0900

    C# 1.0 버전을 사용하여 배열의 내용을 필터링해서 출력하는 프로그램 작성

--- src/CsharpJourney/CsharpJourney/Program.cs
+++ src/CsharpJourney/CsharpJourney/Program.cs
@@ -1,10 +1,25 @@
-﻿namespace CsharpJourney
+﻿using System;
+using System.Collections;
+
+class Program
 {
-    internal class Program
+    static void Main()
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
+        int[] array = { 4, 8, 15, 16, 23 };
+        int[] query = Filter(array, GreaterThanFive);
+        foreach (int value in query) { Console.WriteLine(value); }
     }
+
+    static bool GreaterThanFive(int i) { return i > 5; }
+
+    static int[] Filter(int[] src, Predicate p)
+    {
+        ArrayList dst = new ArrayList();
+        foreach (int value in src) { if (p(value)) dst.Add(value); }
+        int[] result = new int[dst.Count];
+        for (int i = 0; i < result.Length; i++) { result[i] = (int)dst[i]; }
+        return result;
+    }
+
+    delegate bool Predicate(int i);
 }
