34976cdf4121706e7756f19300f66aa524fd3cfc
Park YongJune <redplus@redplus.net>
2025-03-23 21:14:48 +0900

    delegate

--- src/CsharpJourney/CsharpJourney/Program.cs
+++ src/CsharpJourney/CsharpJourney/Program.cs
@@ -1,8 +1,12 @@
 ﻿int[] array = { 4, 8, 15, 16, 23 };
-IEnumerable<int> query = Filter(array, delegate (int i) { return i > 5; });
+var query = Filter(array, GreaterThanFive);
 foreach (int value in query) { WriteLine(value); }
+
+bool GreaterThanFive(int i) { return i > 5; }
 
 IEnumerable<T> Filter<T>(IEnumerable<T> src, Func<T, bool> p)
 {
-    foreach (T value in src) { if (p(value)) yield return value; }
+    List<T> dst = new List<T>();
+    foreach (T value in src) { if (p(value)) dst.Add(value); }
+    return dst.ToArray();
 }
