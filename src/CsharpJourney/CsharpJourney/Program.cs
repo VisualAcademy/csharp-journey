8a435d549ced0e9e0b7df3f5350dc323ae241706
Park YongJune <redplus@redplus.net>
2025-03-23 21:26:17 +0900

    LINQ

--- src/CsharpJourney/CsharpJourney/Program.cs
+++ src/CsharpJourney/CsharpJourney/Program.cs
@@ -1,12 +1,3 @@
 ﻿int[] array = { 4, 8, 15, 16, 23 };
-var query = array.Filter(i => i > 5).Filter(i => i % 2 == 0);
+var query = array.AsQueryable().Where(i => i > 5).Where(i => i % 2 == 0);
 foreach (int value in query) { WriteLine(value); }
-
-static class MyExtensions
-{
-    public static IEnumerable<T> Filter<T>(
-        this IEnumerable<T> src, Func<T, bool> p)
-    {
-        foreach (T value in src) { if (p(value)) yield return value; }
-    }
-}
