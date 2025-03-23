9b717f6002f4bc8ddc7506b255b8ffb58725d235
Park YongJune <redplus@redplus.net>
2025-03-23 21:31:29 +0900

    Optional Parameters

--- src/CsharpJourney/CsharpJourney/Program.cs
+++ src/CsharpJourney/CsharpJourney/Program.cs
@@ -1,6 +1,12 @@
 ﻿int[] array = { 4, 8, 15, 16, 23 };
-var query = from i in array.AsQueryable()
-            where i > 5
-            where i % 2 == 0
-            select i;
-foreach (int value in query) { WriteLine(value); }
+
+ExecuteQuery(array); // printMessage 기본값(true) 사용
+
+void ExecuteQuery(int[] array, bool printMessage = true)
+{
+    var query = array.AsQueryable().Where(i => i > 5 && i % 2 == 0);
+
+    if (printMessage)
+        foreach (int value in query)
+            WriteLine(value); // 기본적으로 값 출력
+}
